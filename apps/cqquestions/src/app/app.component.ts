0665329ed60c167bfd97856411092c7a34409583
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-10-09 14:25:54 +0100

    generate new Nx project for angular apps

---
+++ apps/cqquestions/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cq-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cqquestions';
+}
