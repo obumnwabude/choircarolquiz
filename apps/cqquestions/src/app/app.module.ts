a2efdeb5df53dd7f5e4b91c894de52b4a8668ddf
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-10-11 17:06:18 +0100

    add angular material

--- apps/cqquestions/src/app/app.module.ts
+++ apps/cqquestions/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
