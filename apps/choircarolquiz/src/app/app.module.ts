e36b97ab92c8f4479329ea1eb66b3bb8e39d97ca
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-10-12 13:26:01 +0100

    include app wide spinner in main quiz site

--- apps/choircarolquiz/src/app/app.module.ts
+++ apps/choircarolquiz/src/app/app.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { NgxUiLoaderModule, NgxUiLoaderRouterModule } from 'ngx-ui-loader';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([]),
+    MatToolbarModule,
+    NgxUiLoaderModule,
+    NgxUiLoaderRouterModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
