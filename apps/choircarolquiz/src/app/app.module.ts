7766cc5ba7fe87c38b3c857f297edce507692cae
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-10-13 23:46:33 +0100

    add quiz details to the home page

--- apps/choircarolquiz/src/app/app.module.ts
+++ apps/choircarolquiz/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -23,6 +24,7 @@ const routes: Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     MatButtonModule,
+    MatDividerModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
